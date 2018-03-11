0d37c4594c53965e4bb7cd8d282a0b73ba90043a
Calvin Wei <cwei33@hotmail.com>
2018-03-11 04:51:10 +0000

    Add Manhattan Distance heuristic

---
+++ Astar.js
@@ -0,0 +1,33 @@
+class Astar {
+	constructor() {
+		this.tileCoord = new Map([
+			[0, [0, 0]],
+			[1, [0, 1]],
+			[2, [0, 2]],
+			[3, [1, 0]],
+			[4, [1, 1]],
+			[5, [1, 2]],
+			[6, [2, 0]],
+			[7, [2, 1]],
+			[8, [2, 2]],
+		]);
+	}
+	manhattenDistance(node) {
+		const label = node.label;
+		const goal = '123456780';
+		let currTile;
+		let goalPos;
+		let result = 0;
+		for (let i=0, labelLen=label.length; i<labelLen; i++) {
+			currTile = label[i];
+			if (currTile === '0') {
+				goalPos = 8;
+			} else {
+				goalPos = parseInt(currTile) - 1;
+			}
+			result += Math.abs(this.tileCoord.get(i)[0] - this.tileCoord.get(goalPos)[0]) +
+					  Math.abs(this.tileCoord.get(i)[1] - this.tileCoord.get(goalPos)[1]);
+		}
+		return result;
+	}
+}
